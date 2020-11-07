d68dc6c9ec8e7f9dd341dd7864dc0a35a88cb4bc
SeongyunLim <lsy9699@naver.com>
2020-11-07 14:59:07 +0900

    use redux and hookredux?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {connect, useDispatch, useSelector} from "react-redux";
+import {updateUser} from "./redux/actions";
 
-function App() {
+function App(props) {
+  //subscribe
+  const user = useSelector(state => state.user);
+  const name ="lsy"
+  //dispatch
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>{user}</p>
+      <button onClick={() => dispatch(updateUser(name))}>a</button>
     </div>
   );
 }
-
 export default App;
+// //구독
+// //store의 state를 props로 매핑
+// const mapStateToProps = (state) => ({
+//   //왼쪽은 props, 오른쪽은 store state
+//   user : state.user,
+//   product : state.products
+// });
+//
+// //dispatch
+// //(액션을 디스패치하는)펑션을 props로 매핑
+// const mapActionToProps = (dispatch) => ({
+//   //왼쪽은 props, 오른쪽은 펑션
+//   updateUser : (name)=>dispatch(updateUser(name))
+// })
+// export default connect(mapStateToProps,mapActionToProps)(App);
